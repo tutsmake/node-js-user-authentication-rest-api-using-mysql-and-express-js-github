e3416c0e198994abef552750ac18641927c00f31
Tuts Make <47254033+tutsmake@users.noreply.github.com>
2021-08-18 19:30:03 +0530

    Add files via upload

---
+++ dbConnection.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+  host: 'localhost', // Replace with your host name
+  user: 'root',      // Replace with your database username
+  password: '',      // Replace with your database password
+  database: 'my-node' // // Replace with your database Name
+}); 
+
+conn.connect(function(err) {
+  if (err) throw err;
+  console.log('Database is connected successfully !');
+});
+module.exports = conn;
---
+++ server.js
@@ -0,0 +1,32 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const indexRouter = require('./router.js');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(cors());
+
+app.use('/api', indexRouter);
+
+// Handling Errors
+app.use((err, req, res, next) => {
+    // console.log(err);
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error";
+    res.status(err.statusCode).json({
+      message: err.message,
+    });
+});
+
+app.listen(3000,() => console.log('Server is running on port 3000'));
